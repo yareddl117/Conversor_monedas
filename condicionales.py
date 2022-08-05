ae17aca0fd11944f44c8cfeff767b0d0d0134612
Yared <yared.dominguez117@gmail.com>
2022-08-05 09:45:14 -0500

    intoducción de elif

--- condicionales.py
+++ condicionales.py
@@ -3,3 +3,12 @@ if edad >= 18:
     print("Eres mayor de edad")
 else:
     print("Eres menor de edad") """
+
+numero = int(input("Escribe un número: "))
+
+if numero > 5:
+    print("Es mayor a 5")
+elif numero == 5:
+    print("Es igual a 5")
+else:
+    print("Es menor a 5")
