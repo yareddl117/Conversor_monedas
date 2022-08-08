2fab398dd6f0efb24c12d7bd1118a03c73a7d47d
Yared <yared.dominguez117@gmail.com>
2022-08-07 20:53:41 -0500

    Nuevo archivo donde guardé la función def y omdifiqué el conversor con funciones para acortar el código

--- conversor.py
+++ conversor.py
@@ -1,8 +1,31 @@
-pesos = input("Bienvendio al conversor de monedas. ¿Cuántos pesos mexicanos tienes?: ")
-pesos = float(pesos)
-valor_dolar = 20.45
-dolares = pesos/valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Usted tiene " + dolares + " dolares")
+def conversor(tipo_pesos, valor_dolar):
+    pesos = input("¿Cuántos pesos " + tipo_pesos + " tienes?: ")
+    pesos = float(pesos)
+    dolares = pesos/valor_dolar
+    dolares = round(dolares,2)
+    dolares = str(dolares)
+    print("Usted tiene" + dolares + " dolares.")
+
+menu = """
+Bienvenido al conversor de monedas 🥵
+
+1- Pesos mexicanos
+2- Pesos colombianos 
+3- Pesos argentinos
+
+Elige una opción: """
+
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    conversor("mexicanos", 20)
+elif opcion == 2:
+    conversor("colombianos", 4345)
+elif opcion == 3:
+    conversor("argentinos", 133.47)
+else:
+    print("Ingresa una opción correcta, mamón.")
+
+
 
